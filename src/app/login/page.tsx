9bb0a584410283beb8238554d50a8bd15ca26343
m-aviawan <mfauziavi@gmail.com>
2024-10-17 15:22:45 +0700

    chore: init new project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Formik, Form, Field } from 'formik'
+
+const LoginPage = () => {
+  return (
+    <main className='p-[5vw]'>
+        <h1>Welcome, back!</h1>
+    </main>
+  )
+}
+
+export default LoginPage
